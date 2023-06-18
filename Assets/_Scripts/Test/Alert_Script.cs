5de7260c12ce638e2db3d88413ee79193c62bf01
Pol Maresch <polmaresch@gmail.com>
2023-06-18 17:00:29 +0200

    clean Nomenaclatura

--- Assets/_Scripts/Test/Alert_Script.cs
+++ Assets/_Scripts/Test/Alert_Script.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Alert_Script : MonoBehaviour
 {
-    public Transform target_Camera;
-    public GameObject Player;
-    public bool Active = false;
+    public Transform target_to_Camera;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,23 +15,20 @@ public class Alert_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(gameObject.transform.position - target_Camera.position);
+        transform.LookAt(gameObject.transform.position - target_to_Camera.position);
 
-        transform.position = new Vector3 ( Player.transform.position.x, Player.transform.position.y + 1.5f, Player.transform.position.z );
-        
+        transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + 1.5f, player.transform.position.z );
     }
 
     public void AlertAvaliable(bool active){
-        Debug.Log("enterssssss");
+
         if (active == true)
         {
             gameObject.SetActive(true);
-            Debug.Log("enter2");
         }
        else
         {
             gameObject.SetActive(false);
-            Debug.Log("enter3");
         }
     }
 
