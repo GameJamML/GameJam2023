ec456e599d76f1f514f0e491db1aeffbdf680613
MrIllas <44169046+MrIllas@users.noreply.github.com>
2023-06-20 18:48:00 +0200

    Improving opening

--- Assets/_Scripts/CinematicEvents.cs
+++ Assets/_Scripts/CinematicEvents.cs
@@ -3,26 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class CinematicEvents : MonoBehaviour
 {
-
     private PlayableDirector director;
+
+    private bool cinematicStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
         director = GetComponent<PlayableDirector>();
-        director.Play();
+        director.Pause();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (director.state != PlayState.Playing)
+        if (cinematicStarted)
         {
-            SceneManager.LoadScene(sceneBuildIndex: 1);
+            if (director.state != PlayState.Playing)
+            {
+                SceneManager.LoadScene(sceneBuildIndex: 1);
+            }
         }
-
+        else
+        {
+            if (Input.anyKeyDown)
+            {
+                director.Play();
+                cinematicStarted = true;
+            }
+        }
     }
 }
