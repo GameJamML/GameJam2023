1dd7ce4542d8d61703efeb5fb77411c9e9778e22
Pol Maresch <polmaresch@gmail.com>
2023-06-20 13:16:40 +0200

    Logo Added and Waves Modifier

--- Assets/_Scripts/Ship/Waves.cs
+++ Assets/_Scripts/Ship/Waves.cs
@@ -16,5 +16,33 @@ public class Waves : MonoBehaviour
     {
         float newY = startPos.y + amplitude * Mathf.Sin(frequency * Time.time);
         transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+        
+        // Modificador Oleatge per Keys (Temporal)
+
+        //if (Input.GetKeyDown(KeyCode.I)) levelWaves(1);
+        //if (Input.GetKeyDown(KeyCode.O)) levelWaves(2);
+        //if (Input.GetKeyDown(KeyCode.P)) levelWaves(3);
+
+    }
+
+    public void levelWaves(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                amplitude = 1f;
+                frequency = 2f;
+                break;
+            case 2:
+                amplitude = 1.5f;
+                break;
+            case 3:
+                amplitude = 2f;
+                frequency = 3f;
+                break;
+            default:
+                break;
+        }
+
     }
 }
